e5f7a34459a1e044dffe5ab478d8587559d5f08d
wuhuipeng <wuhuipeng@scentrealm.com>
2023-02-14 21:49:28 +0800

    add protobuf module

---
+++ Yu3zx.Protobuf/ClassTest.cs
@@ -0,0 +1,86 @@
+﻿using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yu3zx.Protobuf
+{
+    [ProtoContract(Name = @"FlyData")]
+    public class FlyData
+    {
+        [ProtoMember(1,IsRequired =true,Name = @"lng",DataFormat = DataFormat.Default)]
+        [global::System.ComponentModel.DefaultValue(0)]
+        public double lng
+        {
+            get;
+            set;
+        }
+        [ProtoMember(2)]
+        public double lat { get; set; }// = 2;//纬度
+        [ProtoMember(3)]
+        public float altitude { get; set; }// = 3;//海拔高度(椭球高)(单位:m)
+        [ProtoMember(4)]
+        public float ultrasonic { get; set; }// = 4;//相对高度
+        [ProtoMember(5)]
+        public float pitch { get; set; }// = 5;//俯仰角
+        [ProtoMember(6)]
+        public float roll { get; set; }// = 6;//横滚角
+        [ProtoMember(7)]
+        public float yaw { get; set; }// = 7;//偏航角
+    }
+    /// <summary>
+    /// 测试方法
+    /// </summary>
+    public class ClassTest
+    {
+        public static FlyData GetFlyData(byte[] bFly)
+        {
+            FlyData fly = ProtobufHelper.DeSerialize<FlyData>(bFly);
+            if(fly != null)
+            {
+                return fly;
+            }
+            else
+            {
+                return new FlyData() { altitude = 0, lat = 123.433, lng = 32.5 };
+            }
+        }
+
+        public static string GetFlyStrData(FlyData flyData)
+        {
+            string flyStr = ProtobufHelper.Serialize(flyData);
+            if(string.IsNullOrEmpty(flyStr))
+            {
+                flyStr = "";
+            }
+            return flyStr;
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            FlyData flyData = new FlyData();
+            flyData.altitude = 1.0f;
+            flyData.lat = 123.58f;
+            flyData.lng = 36.21f;
+            flyData.pitch = 1.0f;
+            flyData.roll = 2.01f;
+            flyData.ultrasonic = 3.0f;
+            flyData.yaw = 1;
+
+            byte[] sss = ProtobufHelper.Serialize2Bytes<FlyData>(flyData);
+            Console.WriteLine("Data:" + string.Join(" ", sss.Select(x=>x.ToString("X2"))));
+
+            byte[] bData = sss;
+            var vLfy = GetFlyData(bData);
+            if(vLfy != null)
+            {
+                Console.WriteLine("Obj:" + vLfy.lat);
+                Console.WriteLine("Obj:" + vLfy.lng);
+                Console.WriteLine("Obj:" + vLfy.ultrasonic);
+            }
+        }
+    }
+}
---
+++ Yu3zx.Protobuf/ProtobufHelper.cs
@@ -0,0 +1,73 @@
+﻿using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yu3zx.Protobuf
+{
+    public class ProtobufHelper
+    {
+        /// <summary>
+        /// 序列化
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public static string Serialize<T>(T t)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                Serializer.Serialize<T>(ms, t);
+                return Encoding.UTF8.GetString(ms.ToArray());
+            }
+        }
+
+        /// <summary>
+        /// 序列化
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public static byte[] Serialize2Bytes<T>(T t)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                Serializer.Serialize<T>(ms, t);
+                return ms.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// 反序列化
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public static T DeSerialize<T>(string content)
+        {
+            using (MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes(content)))
+            {
+                T t = Serializer.Deserialize<T>(ms);
+                return t;
+            }
+        }
+
+        /// <summary>
+        /// 反序列化
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="content"></param>
+        /// <returns></returns>
+        public static T DeSerialize<T>(byte[] content)
+        {
+            using (MemoryStream ms = new MemoryStream(content))
+            {
+                T t = Serializer.Deserialize<T>(ms);
+                return t;
+            }
+        }
+    }
+}
