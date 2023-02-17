60a6586422ab94deaa1d6d9e36ec70e7f9ba0f99
wuhuipeng <wuhuipeng@scentrealm.com>
2023-02-17 10:05:36 +0800

    Util module edit

--- Yu3zx.Util/StringUtil.cs
+++ Yu3zx.Util/StringUtil.cs
@@ -209,8 +209,19 @@ namespace Yu3zx.Util
             endIndex = -1;
             return false;
         }
-        #endregion   
+        #endregion
 
+        /// <summary>
+        /// 阿拉伯数字转换为汉字大写
+        /// </summary>
+        /// <param name="x"></param>
+        /// <returns></returns>
+        public static string ConvertToChinese(double x)
+        {
+            string s = x.ToString("#L#E#D#C#K#E#D#C#J#E#D#C#I#E#D#C#H#E#D#C#G#E#D#C#F#E#D#C#.0B0A");
+            string d = Regex.Replace(s, @"((?<=-|^)[^1-9]*)|((?'z'0)[0A-E]*((?=[1-9])|(?'-z'(?=[F-L\.]|$))))|((?'b'[F-L])(?'z'0)[0A-L]*((?=[1-9])|(?'-z'(?=[\.]|$))))", "${b}${z}");
+            return Regex.Replace(d, ".", m => "负元空零壹贰叁肆伍陆柒捌玖空空空空空空空分角拾佰仟萬億兆京垓秭穰"[m.Value[0] - '-'].ToString());
+        }
 
         public static bool IsValidIP(string ip)
         {
