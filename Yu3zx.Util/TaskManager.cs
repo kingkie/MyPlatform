acfc6488a68f951c3d9703635a8b694083668bb3
wuhuipeng <wuhuipeng@scentrealm.com>
2023-03-29 09:20:45 +0800

    add util classheler

--- Yu3zx.Util/TaskManager.cs
+++ Yu3zx.Util/TaskManager.cs
@@ -47,5 +47,25 @@ namespace Yu3zx.Util
             });
             taskWait.Start();
         }
+
+        /// <summary>
+        /// 开始异步任务
+        /// </summary>
+        /// <param name="action"></param>
+        public static void StartTask(Action action)
+        {
+            try
+            {
+                Action newAction = () =>
+                { };
+                newAction += action;
+                Task task = new Task(newAction);
+                task.Start();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
     }
 }
